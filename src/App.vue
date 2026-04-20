677f40c3ef2d219860784caed2830e830938772e
vuongquocanh973-cell <vuongquocanh973@gmail.com>
2026-04-20 13:31:00 +0700

    landautiencuatoi

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const message = ref('')
+
+onMounted(async () => {
+  try {
+    const res = await fetch('http://localhost:5239')
+    message.value = await res.text()
+  } catch (err) {
+    console.error(err)
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>Vue + ASP.NET CI/CD Demo</h1>
+    <p>Backend says: {{ message }}</p>
+  </div>
+</template>
